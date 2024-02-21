ae73860c2689487a95453ddee880735806eb5234
eduardorossetti <edurossettimelo@hotmail.com>
2024-02-21 18:16:38 -0300

    add: Animation and accessibility

--- README.md
+++ README.md
@@ -1,44 +1,22 @@
 # Histórico de Alterações
 
+## APP
+
+### Adições
+
+- Adicionado animações e melhorias na acessibilidade
+
 ## 20/02/2024
 
-## APP
+## APP 1.0.1
 
 ### Adições
 
 - Adicionado componentes do formulário
-
-## 18/02/2024
-
-## APP
-
-### Adições
-
 - Adicionado abas
-
-## 18/02/2024
-
-## APP
-
-### Adições
-
 - Adicionado abas
-
-## 17/02/2024
-
-## APP
-
-### Adições
-
 - Adicionado campo de busca e logo
 - Adicionado menu de navegação
 - Adicionado perfil do usuário
-
-## 16/02/2024
-
-## APP
-
-### Adições
-
 - Adicionado configurações do ESlint e Prettier
 - Adicionado estrutura do layout
--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { motion } from 'framer-motion'
 import * as Tabs from '@radix-ui/react-tabs'
 
@@ -11,10 +12,12 @@ interface TabItemProps {
 export function TabItem({ title, value, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
-      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=true]:text-violet-700"
+      className=":px-1 group relative pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-700"
       value={value}
     >
-      <span>{title}</span>
+      <span className="rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+        {title}
+      </span>
       {isSelected && (
         <motion.div
           layoutId="activeTab"
