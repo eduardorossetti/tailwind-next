52040bf175cbc9d2f84e63724b6bc011f8dcc421
eduardorossetti <edurossettimelo@hotmail.com>
2024-02-25 19:00:47 -0300

    add: dark theme

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,27 +1,28 @@
 'use client'
 
-import { motion } from 'framer-motion'
 import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
 
-interface TabItemProps {
+export interface TabItemProps {
   value: string
   title: string
   isSelected?: boolean
 }
 
-export function TabItem({ title, value, isSelected = false }: TabItemProps) {
+export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
-      className=":px-1 group relative pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-700"
       value={value}
+      className=":px-1 group relative pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-700 dark:text-zinc-300 dark:hover:text-violet-300 dark:data-[state=active]:text-violet-300"
     >
       <span className="whitespace-nowrap rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
         {title}
       </span>
+
       {isSelected && (
         <motion.div
           layoutId="activeTab"
-          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700 dark:bg-violet-300"
         />
       )}
     </Tabs.Trigger>
--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,5 +1,6 @@
 import { LogOut } from 'lucide-react'
 import { Button } from '../Button'
+import { twMerge } from 'tailwind-merge'
 
 /* eslint-disable @next/next/no-img-element */
 export function Profile() {
@@ -11,15 +12,25 @@ export function Profile() {
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="truncate text-sm font-semibold text-zinc-700">
+        <span
+          className={twMerge(
+            'truncate text-sm font-semibold text-zinc-700',
+            'dark:text-zinc-100',
+          )}
+        >
           Eduardo Rossetti dos Santos Melo
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span
+          className={twMerge(
+            'truncate text-sm text-zinc-500',
+            'dark:text-zinc-400',
+          )}
+        >
           edurossettimelo@hotmail.com
         </span>
       </div>
       <Button type="button" variant="ghost">
-        <LogOut className="h-5 w-5 text-zinc-500" />
+        <LogOut className="h-5 w-5" />
       </Button>
     </div>
   )
